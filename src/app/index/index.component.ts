18b0f3008bcfcbb75ea71f214d48d88ae309ce79
Christian Martinez <cgmartinez11142@gmail.com>
2017-05-15 15:54:03 -0700

    get data retrieval from firebase working

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AsteroidService } from '../asteroid.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  styleUrls: ['./index.component.css'],
+  providers: [ AsteroidService ]
 })
 export class IndexComponent implements OnInit {
+  asteroids: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private asteroidService: AsteroidService) { }
 
   ngOnInit() {
+    this.asteroids = this.asteroidService.getAsteroids();
   }
 
 }
