c59930125cd9f507eb68e68075845134d4062b23
Shane Stafford <shanestafford@live.com>
2017-05-17 11:38:45 -0700

    successfully get each name in console log

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AsteroidService } from '../asteroid.service';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-test',
@@ -10,10 +12,15 @@ import { AsteroidService } from '../asteroid.service';
 export class TestComponent implements OnInit {
   callData = [];
 
-  constructor(private asteroidService: AsteroidService) { }
+  constructor(private asteroidService: AsteroidService, private http: Http) { }
 
   ngOnInit() {
-    this.asteroidService.getData().subscribe(data => this.callData = data);
-  }
+    this.http.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-05-17&end_date=2017-05-17&api_key=SafUQrFLoCbNXkGFaT4xvfbWPUZSpCxcOV0G2PPI`)
+    .map((res:Response) => res.json().near_earth_objects["2017-05-17"])
+    .subscribe(data => {
+      this.callData = data;
+      this.callData.forEach(n => console.log(n.name));
+  });
 
+}
 }
